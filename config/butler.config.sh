af3066e4dab1e91491cac1d4039c9b0dbbe0f27e
palantir-team <byrwa@palantir.net>
2016-04-06 13:51:01 -0500

    run sh script as postinstall

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -3,7 +3,6 @@
 echo -e "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
 read project_repo
 echo "defaults.repo = \"$project_repo\";" >> butler.defaults.js
-;;
 
 module.exports = defaults;
 
