b613f84c57720bd2ebaaf9fc237c5515dd804b81
palantir-team <byrwa@palantir.net>
2016-04-06 14:20:02 -0500

    super simple shell script

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-EOF
+echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
+read project_repo
+echo "defaults.repo = \"$project_repo\";" >> butler.defaults.js
 
-	echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
-	read project_repo
-	echo "defaults.repo = \"$project_repo\";" >> butler.defaults.js
-
-	;;
-
-esac
-cat >> bulter.defaults.js << EOF
-
-module.exports = butler.defaults.js;
-EOF
-	;;
+echo "Thanks for using Butler!"
