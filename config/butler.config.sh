86f540163527d80e9e4282bec04590a7097df293
palantir-team <byrwa@palantir.net>
2016-04-06 14:04:23 -0500

    update config.sh

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo -e "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
+EOF
+
+echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
 read project_repo
 echo "defaults.repo = \"$project_repo\";" >> butler.defaults.js
+
 module.exports = butler.defaults.js;
+EOF
+
+esac
