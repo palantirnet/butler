b8ec15e5554a80b198995ce91e2cc7e1c23821cd
palantir-team <byrwa@palantir.net>
2016-04-06 13:55:59 -0500

    set butler defaults as the export place

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -3,8 +3,7 @@
 echo -e "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
 read project_repo
 echo "defaults.repo = \"$project_repo\";" >> butler.defaults.js
-
-module.exports = defaults;
+module.exports = butler.defaults.js;
 
 esac
 
