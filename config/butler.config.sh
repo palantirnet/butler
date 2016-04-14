f27178887d61ff2b44915c094bca36894de0670f
palantir-team <byrwa@palantir.net>
2016-04-14 18:44:05 -0500

    find the main declaration and put it after that

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -2,13 +2,15 @@
 
 config = "../../conf/butler.defaults.js"
 json = "../../package.json"
+scripts = '"scripts": {"butler": "gulp --gulpfile node_modules/butler/gulpfile.js develop","linting": "gulp --gulpfile node_modules/butler/gulpfile.js test",	"deploy": "gulp --gulpfile node_modules/butler/gulpfile.js deploy"},'
+match = '"main": "index.js",'
 
 echo "// Project-specific Butler configuration." > $config
 echo "" >> $config
 
 if [ -r $json ]
 then
-  sed -i '6i\"scripts": {"butler": "gulp --gulpfile node_modules/butler/gulpfile.js develop","linting": "gulp --gulpfile node_modules/butler/gulpfile.js test",	"deploy": "gulp --gulpfile node_modules/butler/gulpfile.js deploy"},' $json
+	sed -i "s/$match/$match\n$scripts/" $json
 fi
 
 if [ -r $config ]
