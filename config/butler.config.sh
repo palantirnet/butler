3736046a849b0a1dc7a9b159f4ecac8a8aca992d
palantir-team <byrwa@palantir.net>
2016-04-14 17:29:32 -0500

    test write scripts to package.json

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -3,8 +3,17 @@
 echo "// Project-specific Butler configuration." > ../../conf/butler.defaults.js
 echo "" >> ../../conf/butler.defaults.js
 
+if [ -r ././../package.json ]
+then
+  echo ""scripts": {
+	"butler": "gulp --gulpfile node_modules/butler/gulpfile.js develop",
+	"linting": "gulp --gulpfile node_modules/butler/gulpfile.js test",
+	"deploy": "gulp --gulpfile node_modules/butler/gulpfile.js deploy"
+},"
+fi
+
 echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
 read project_repo
 echo "defaults.repo = \"$project_repo\";" >> ../../conf/butler.defaults.js
 
-echo "Thanks for using Butler!"
+echo "Save your project wide settings by committing your new package.json and conf/butler.defaults.js. Thanks for using Butler!"
