4979c1c55ed5035145fb2b70ea020c22f40069e0
palantir-team <byrwa@palantir.net>
2016-04-14 17:56:15 -0500

    reset to append to package.json instead of overwrite

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -5,7 +5,7 @@ echo "" >> ../../conf/butler.defaults.js
 
 if [ -r ../../package.json ]
 then
-  echo '"scripts": {"butler": "gulp --gulpfile node_modules/butler/gulpfile.js develop","linting": "gulp --gulpfile node_modules/butler/gulpfile.js test",	"deploy": "gulp --gulpfile node_modules/butler/gulpfile.js deploy"},' > ../../package.json
+  echo '"scripts": {"butler": "gulp --gulpfile node_modules/butler/gulpfile.js develop","linting": "gulp --gulpfile node_modules/butler/gulpfile.js test",	"deploy": "gulp --gulpfile node_modules/butler/gulpfile.js deploy"},' >> ../../package.json
 fi
 
 echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
