178d360ae26830f94dd67a2be942255bc22858f3
Bec White <white@palantir.net>
2016-04-15 15:31:05 -0500

    If a project already has conf/butler.defaults.js, don't try to re-create it. (#27)

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "// Project-specific Butler configuration." > ../../conf/butler.defaults.js
-echo "" >> ../../conf/butler.defaults.js
+if [ ! -e ../../conf/butler.defaults.js ]; then
+  echo "// Project-specific Butler configuration." > ../../conf/butler.defaults.js
+  echo "" >> ../../conf/butler.defaults.js
 
-echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
-read project_repo
-echo "defaults.repo = \"$project_repo\";" >> ../../conf/butler.defaults.js
+  echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
+  read project_repo
+  echo "defaults.repo = \"$project_repo\";" >> ../../conf/butler.defaults.js
+fi;
 
 echo "Thanks for using Butler!"
