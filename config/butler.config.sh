98fdae3850df486f4568e12c4b1c063ff47ee0d7
Bec White <white@palantir.net>
2016-11-29 21:15:58 -0600

    Add spress config on install.

--- config/butler.config.sh
+++ config/butler.config.sh
@@ -8,6 +8,22 @@ if [ ! -e ../../conf/butler.defaults.js ]; then
   echo "Please provide the repository link for this project. Eg: https://github.com/palantirnet/butler.git"
   read project_repo
   echo "overrides.repo = \"$project_repo\";" >> ../../conf/butler.defaults.js
+
+  echo "Are you using spress? (y/n)"
+  read using_spress
+
+  if [[ "$using_spress" == "y" ]]; then
+    echo "overrides.develop_tasks = ['sass', 'spress', 'watch'];" >> ../../conf/butler.defaults.js
+    echo "overrides.scss = ['../../src/sass/*.scss', '../../src/sass/**/*.scss'];" >> ../../conf/butler.defaults.js
+    echo "overrides.css = '../../src/content/assets/css/';" >> ../../conf/butler.defaults.js
+    echo "overrides.template_files = ['../../src/*.html', '../../src/**/*.html'];" >> ../../conf/butler.defaults.js
+    echo "overrides.output_dev = '../../build';" >> ../../conf/butler.defaults.js
+    echo "overrides.html_files = ['../../build/*.html', '../../build/**/*.html'];" >> ../../conf/butler.defaults.js
+    echo "overrides.output_prod = '../../build/**/*';" >> ../../conf/butler.defaults.js
+  fi;
+
+  echo "" >> ../../conf/butler.defaults.js
+  echo "module.exports = overrides;" >> ../../conf/butler.defaults.js
 fi;
 
 echo "Thanks for using Butler!"
